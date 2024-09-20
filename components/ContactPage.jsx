e3b052daf4b4c36c2dd563e8e210dbf72837ec49
Ryzn <rahulblue17@gmail.com>
2024-09-20 20:36:36 +0530

    feat: Add useParams hook to ContactPage component

--- components/ContactPage.jsx
+++ components/ContactPage.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 export const ContactPage = () => {
+  const params = useParams()
+  console.log(params)
   return (
     <h1>Contact us</h1>
   )
