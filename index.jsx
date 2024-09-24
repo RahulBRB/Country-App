d447593b58f85dd863dcb5fe1f8fcab4713cbf52
Ryzn <rahulblue17@gmail.com>
2024-09-24 17:17:29 +0530

    Changed routing method.

--- index.jsx
+++ index.jsx
@@ -1,37 +1,31 @@
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
 import { ContactPage } from "./components/ContactPage";
 import Home from "./components/Home";
 import Error from "./components/Error";
 import CountryDetail from "./components/CountryDetail";
+import CountryDetails from "./components/CountryDetails"; // Assuming you have these components
+import CountryPopulation from "./components/CountryPopulation"; // Assuming you have these components
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <Error />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
+const RootApp = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<ContactPage />} />
+          {/* Nested Route under Country */}
+          <Route path="/:country" element={<CountryDetail />}>
+            <Route path="details" element={<CountryDetails />} />
+            <Route path="population" element={<CountryPopulation />} />
+          </Route>
+        </Route>
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
+  );
+};
 
-      {
-        path: "/contact",
-        element: <ContactPage />,
-      },
-
-      {
-        path: '/:country',
-        element: <CountryDetail />,
-      }
-    ],
-  },
-]);
 const root = createRoot(document.getElementById("root"));
-root.render(
-  <>
-    <RouterProvider router={router} />
-  </>
-);
+root.render(<RootApp />);
