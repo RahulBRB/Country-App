b5e643926e612c602777ffde12501b417b136ff9
Ryzn <rahulblue17@gmail.com>
2024-09-27 18:06:39 +0530

    Shimmer effect style fixed.

--- components/CountriesListShimmer.jsx
+++ components/CountriesListShimmer.jsx
@@ -3,9 +3,9 @@ import "./CountriesListShimmer.css";
 
 export default function CountriesListShimmer() {
   // new Array(10).fill('')
-  
+
   return (
-    <div className="contries-container">
+    <div className="countries-container">
       {Array.from({length:10}).map((el,i)=>{
         return (
             <div key={i} className="country-card shimmer-card">             
