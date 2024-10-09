5af0a82c13d5c84d20f7dc6d1b8f342d7f47dbd9
Ryzn <rahulblue17@gmail.com>
2024-10-09 15:44:04 +0530

    refactor: Update CountriesList and CountryCard components to pass country data through props

--- components/CountriesList.jsx
+++ components/CountriesList.jsx
@@ -30,6 +30,7 @@ export default function CountriesList({ query }) {
               population={country.population.toLocaleString("en-IN")}
               region={country.region}
               capital={country.capital?.[0]}
+              data={country}
             />
           ))}
       </div>}
