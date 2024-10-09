c4d06d0ae913f5cad04417a34c068c14521d958c
Ryzn <rahulblue17@gmail.com>
2024-10-09 17:09:24 +0530

    Functionality implemented.

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,20 +1,34 @@
 import React, { useState } from "react";
 
 export default function Header() {
-  const [isDark, setIsDark] = useState(false);
+  const [isDark, setIsDark] = useState(
+    JSON.parse(localStorage.getItem("isDark"))
+  );
 
+  if(isDark){
+    document.body.classList.add("dark");
+  }else {
+    document.body.classList.remove("dark");
+  }
+  
   return (
     <header className="header-container">
       <div className="header-content">
         <h2 className="title">
           <a href="/">Where in the world?</a>
         </h2>
-        <p className="theme-changer" onClick={()=>{
-          document.body.classList.toggle("dark")
-          setIsDark(!isDark);
-        }}>
-          <i className={`fa-solid fa-${isDark? 'moon' : 'sun'}`} />
-          &nbsp;&nbsp;{!isDark? 'Light' : 'Dark'} Mode
+        <p
+          className="theme-changer"
+          onClick={() => {
+            document.body.classList.toggle("dark");
+            setIsDark(!isDark);
+
+            // Save the theme preference in local storage
+            localStorage.setItem("isDark", !isDark);
+          }}
+        >
+          <i className={`fa-solid fa-${!isDark ? "moon" : "sun"}`} />
+          &nbsp;&nbsp;{isDark ? "Light" : "Dark"} Mode
         </p>
       </div>
     </header>
