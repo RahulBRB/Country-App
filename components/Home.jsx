4e76380ac57889384b7fee4bcdc00eef88a2b799
Ryzn <rahulblue17@gmail.com>
2024-10-14 17:25:48 +0530

    Height and width both added.

--- components/Home.jsx
+++ components/Home.jsx
@@ -2,27 +2,33 @@ import React, { useContext, useEffect, useState } from "react";
 import Searchbar from "./Searchbar";
 import SelectMenu from "./SelectMenu";
 import CountriesList from "./CountriesList";
-import {ThemeContext} from "../contexts/ThemeContext";
+import { ThemeContext } from "../contexts/ThemeContext";
 export default function Home() {
-    const [query, setQuery] = useState("");
-    const [isDark] = useContext(ThemeContext);
-    const [width, setWidth] = useState(
-      window.innerWidth
-    )
-    useEffect(()=>{
-      window.addEventListener('resize', ()=>{
-       setWidth(window.innerWidth)
-      })
-    }, [])
+  const [query, setQuery] = useState("");
+  const [isDark] = useContext(ThemeContext);
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    });
+  }, []);
 
-    return (
-    <main className={`${isDark? 'dark' : ''}`}>
-    <div className="search-filter-container">
-      <Searchbar setQuery={setQuery} />
-      <SelectMenu />
-    </div>
-    <h1 style={{textAlign: 'center'}}>{width}</h1>
-    <CountriesList query={query} />
-  </main>
-  )
+  return (
+    <main className={`${isDark ? "dark" : ""}`}>
+      <div className="search-filter-container">
+        <Searchbar setQuery={setQuery} />
+        <SelectMenu />
+      </div>
+      <h1 style={{ textAlign: "center" }}>
+        {windowSize.width} X {windowSize.height}
+      </h1>
+      <CountriesList query={query} />
+    </main>
+  );
 }
