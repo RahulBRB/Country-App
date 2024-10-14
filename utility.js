a4cf997145b8126da5eb531e616a943b45fc5e25
Ryzn <rahulblue17@gmail.com>
2024-10-14 17:33:08 +0530

    feat: Add getWindowSize utility function

---
+++ utility.js
@@ -0,0 +1,16 @@
+export function getWindowSize(){
+    const [windowSize, setWindowSize] = useState({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+      useEffect(() => {
+        window.addEventListener("resize", () => {
+          setWindowSize({
+            width: window.innerWidth,
+            height: window.innerHeight,
+          });
+        });
+      }, []);
+      return windowSize
+
+}
